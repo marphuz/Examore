40f8f6ec22b535c65cd7dbe3faa9e4e344179471
Filippo Leonelli <300717@studenti.unimore.it>
2024-11-04 19:04:01 +0100

    filtri appelli e vista calendario implementata

--- main/views.py
+++ main/views.py
@@ -1,11 +1,42 @@
+from types import NoneType
+
 from allauth.socialaccount.models import SocialAccount, SocialToken
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import LoginForm, RegisterForm
 from django.contrib.auth import login, authenticate
 from .utils import create_google_calendar_event
 from scraping_creatingmodels import create, scraping
-from .models import Facolta,Esame,AppelloEsame,Aula, DisponibilitaOraria
+from .models import Facolta, Esame, AppelloEsame, Aula, DisponibilitaOraria
 from datetime import datetime, date
+
+
+# FILTER FUNCTIONS:
+
+def filterAppelli(anno_esame, periodo_esame, data_stringa):
+    if anno_esame is "":
+        anno_esame = 0
+    if periodo_esame is "":
+        periodo_esame = 0
+    if anno_esame is not "" and periodo_esame is "":
+        anno_esame = int(anno_esame)
+        periodo_esame = 0
+    elif anno_esame is "" and periodo_esame is not "":
+        anno_esame = 0
+        periodo_esame = int(periodo_esame)
+
+    appelli_attuali = None
+
+    if anno_esame == 0 and periodo_esame == 0:
+        appelli_attuali = AppelloEsame.objects.filter(data__icontains=data_stringa)
+    elif anno_esame != 0 and periodo_esame == 0:
+        appelli_attuali = AppelloEsame.objects.filter(data__icontains=data_stringa, esame__anno=anno_esame)
+    elif anno_esame == 0 and periodo_esame != 0:
+        appelli_attuali = AppelloEsame.objects.filter(data__icontains=data_stringa, esame__semestre=periodo_esame)
+    else:
+        appelli_attuali = AppelloEsame.objects.filter(data__icontains=data_stringa, esame__anno=anno_esame,
+                                                      esame__semestre=periodo_esame)
+
+    return appelli_attuali
 
 
 # Create your views here.
@@ -29,7 +60,16 @@ def esami_per_facolta(request, facolta_id):
 
 
 def calendar(request, facolta_id=None, day=None, month=None, year=None):
-    aule = Aula.objects.all()
+    aule = None
+    aule_disponibilita = {}
+    anno_esame = request.GET.get('anno_esame')
+    periodo_esame = request.GET.get('periodo_esame')
+    aula_visible = request.GET.get('aula_visible')
+    if aula_visible is not "":
+        aule = Aula.objects.all()
+    else:
+        aule = Aula.objects.filter(nome=aula_visible)
+
     if day is None and month is None and year is None:
         today = datetime.today()
         day = today.strftime("%d")
@@ -37,16 +77,19 @@ def calendar(request, facolta_id=None, day=None, month=None, year=None):
         year = today.strftime("%Y")
 
     data_attuale = date(int(year), int(month), int(day))
+    data_stringa = data_attuale.strftime("%d/%m/%Y")
 
-    aule_disponibilita = {}
-
+    appelli_attuali = filterAppelli(anno_esame, periodo_esame, data_stringa)
     for a in aule:
         aule_disponibilita[a] = DisponibilitaOraria.objects.filter(aula=a, data=data_attuale)
 
     context = {
         "aule": aule,
         "data_attuale": data_attuale,
-        "aule_disponibilita": aule_disponibilita
+        "aule_disponibilita": aule_disponibilita,
+        "appelli": appelli_attuali,
+        "anno_esame": anno_esame,
+        "periodo_esame": periodo_esame
     }
     return render(request, "main/cl.html", context)
 
